890950be5fef09b06690f2e7d344381f140e273f
vanquanajc <vanquan.ajc@gmail.com>
2020-05-05 12:24:33 +0700

    Update account_example.py

--- accounts_example.py
+++ accounts_example.py
@@ -1,9 +1,9 @@
 import requests
 import json
 
-spreadsheetId = '1zEZl0Do3jiWNsT03sdicwYeCP11rOe1m-Wv66URG-xM'
-apiKey = 'AIzaSyAzr3ZV-eJ58M-VJmACMZWDr-Mu_twqflA'
-_range = 'A2'
+spreadsheetId = ''
+apiKey = ''
+_range = ''
 
 url = f'https://sheets.googleapis.com/v4/spreadsheets/{spreadsheetId}/values/{_range}?key={apiKey}'
 
